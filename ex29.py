10d8a63bfea16c6706e0e4cae8cde50a39031783
jasminpy <49554549+jasminpy@users.noreply.github.com>
2020-04-25 17:40:24 +0430

    Create ex29.py

---
+++ ex29.py
@@ -0,0 +1,25 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats")
+
+if people > cats:
+    print("not many cats!")
+
+if people < dogs:
+    print("the world is drooled")
+
+if people > dogs:
+    print("the world is dry")
+
+dogs += 5
+if people >= dogs:
+    print("people are greater")
+
+if people <= dogs:
+    print("dogs are greater")
+
+if people == dogs:
+    print("people and dogs are equal")
